23c8887bf82d9f6535b040f12a6d9ff6ba0ad63e
Alex <aleksandar.ivanov@estafet.com>
2022-06-15 13:45:05 +0300

    Implemented AuthFilter, WebClientConfig, api-gateway working

---
+++ src/main/java/com/codelikealexito/cloud/gateway/dto/UserDto.java
@@ -0,0 +1,17 @@
+package com.codelikealexito.cloud.gateway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class UserDto {
+
+    private long id;
+    private String username;
+    private String token;
+}
